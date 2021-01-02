2a18f8f5bb0820f747025a523b0623e922c7fa08
Ochuko Ekrresa <kelvinekrresa@gmail.com>
2021-01-02 14:11:58 +0100

    user model

---
+++ server/src/api/models/user.ts
@@ -0,0 +1,31 @@
+import { Document, model, Schema } from 'mongoose';
+
+import { IUser } from '../types';
+
+const userSchema = new Schema(
+	{
+		firstName: { type: String, required: true },
+		lastName: { type: String, required: true },
+		email: {
+			type: String,
+			required: true,
+			unique: true,
+		},
+		emailVerified: { type: Boolean, default: false },
+		enabled: { type: Boolean, default: false },
+		lastLogin: { type: Date, default: null },
+		phoneNumber: { type: String, default: null },
+		phoneNumberVerified: { type: Boolean, default: false },
+		password: { type: String, required: true },
+		roles: [
+			{
+				type: String,
+				enum: ['customer', 'dispatch', 'vendor'],
+				default: 'customer',
+			},
+		],
+	},
+	{ timestamps: true }
+);
+
+export const User = model<IUser & Document>('User', userSchema);
