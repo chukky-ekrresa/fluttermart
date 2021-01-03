e4a20e5b6ff345a52b909b647e068595cc44aa55
Ochuko Ekrresa <kelvinekrresa@gmail.com>
2021-01-03 22:40:23 +0100

    Setup email client

--- server/src/config/env.ts
+++ server/src/config/env.ts
@@ -7,6 +7,21 @@ dotenv.config();
 const envVarsSchema = Joi.object({
 	ACCESS_TOKEN_SECRET: Joi.string().required(),
 	DATABASE_URL: Joi.string().required(),
+	MAIL_SENDER: Joi.string().when('NODE_ENV', {
+		is: Joi.string().equal('production', 'development'),
+		then: Joi.required(),
+		otherwise: Joi.optional(),
+	}),
+	MJ_APIKEY_PUBLIC: Joi.string().when('NODE_ENV', {
+		is: Joi.string().equal('production', 'development'),
+		then: Joi.required(),
+		otherwise: Joi.optional(),
+	}),
+	MJ_APIKEY_PRIVATE: Joi.string().when('NODE_ENV', {
+		is: Joi.string().equal('production', 'development'),
+		then: Joi.required(),
+		otherwise: Joi.optional(),
+	}),
 	NODE_ENV: Joi.string(),
 	PORT: Joi.string(),
 	BASE_URL: Joi.string(),
--- server/typings/index.d.ts
+++ server/typings/index.d.ts
@@ -3,6 +3,9 @@ declare namespace NodeJS {
 		ACCESS_TOKEN_SECRET: string;
 		DATABASE_TEST_URL: string;
 		DATABASE_URL: string;
+		MAIL_SENDER: string;
+		MJ_APIKEY_PUBLIC: string;
+		MJ_APIKEY_PRIVATE: string;
 		NODE_ENV: 'development' | 'production' | 'test';
 		PORT: string;
 	}
