b4f2a9026e1bc6fddbdf4237df46f1c3e886415f
Ochuko Ekrresa <kelvinekrresa@gmail.com>
2021-01-05 18:57:01 +0100

    Add route to check api status

--- server/src/api/routes/index.ts
+++ server/src/api/routes/index.ts
@@ -4,6 +4,10 @@ import AuthRouter from './auth';
 
 const router = Router();
 
+router.get('/', (_req, res) => {
+	res.status(200).send('Fluttermart backend service running');
+});
+
 router.use('/auth', AuthRouter);
 
 export default router;
