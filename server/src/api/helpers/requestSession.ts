48db9161bda7f1f7099607a6885c20ca57ee569b
Ochuko Ekrresa <kelvinekrresa@gmail.com>
2021-01-06 19:59:52 +0100

    Cache logged in user info throughout request lifetime

---
+++ server/src/api/helpers/requestSession.ts
@@ -0,0 +1,15 @@
+import { createNamespace } from 'cls-hooked';
+import { IUser } from '../types';
+
+// This module sets up a session to keep info about a loggedIn user
+// throughout the lifetime of a request
+export const session = createNamespace('request-session');
+const LOGGED_IN_USER = 'loggedInUser';
+
+export function setLoggedInUser(payload: Partial<IUser>) {
+	session.set(LOGGED_IN_USER, payload);
+}
+
+export function getLoggedInUser() {
+	return session.get(LOGGED_IN_USER);
+}
