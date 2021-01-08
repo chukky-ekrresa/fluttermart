70a89e8975b9fe7ec76d0ace81682db136a25c33
Ochuko Ekrresa <kelvinekrresa@gmail.com>
2021-01-08 20:17:48 +0100

    Add trim to string validations

--- server/src/api/validations/product.ts
+++ server/src/api/validations/product.ts
@@ -2,14 +2,17 @@ import { Joi, Segments } from 'celebrate';
 
 export const NEW_PRODUCT = {
 	[Segments.BODY]: Joi.object().keys({
-		colour: Joi.string(),
-		discount: Joi.string().regex(/^\s*-?0{1}(\.\d{2})?\s*$/),
-		name: Joi.string().required(),
+		colour: Joi.string().trim(),
+		discount: Joi.string()
+			.trim()
+			.regex(/^\s*-?0{1}(\.\d{2})?\s*$/),
+		name: Joi.string().trim().required(),
 		price: Joi.string()
+			.trim()
 			.regex(/^\s*-?\d+(\.\d{2})?\s*$/)
 			.required(),
 		quantity: Joi.number().positive().min(0).required(),
-		size: Joi.string(),
-		summary: Joi.string(),
+		size: Joi.string().trim(),
+		summary: Joi.string().trim(),
 	}),
 };
