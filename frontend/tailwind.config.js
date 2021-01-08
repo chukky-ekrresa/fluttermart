e138d1ac700d2e5f64ab6a7054fe222e07fbf80d
EstherSeyi <sheyiogundijo@gmail.com>
2021-01-08 08:01:02 +0100

    configures tailwind

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+	purge: [],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		fontSize: {
+			14: '0.875rem',
+			24: '1.5rem',
+		},
+
+		extend: {
+			fontFamily: {
+				quicksand: ['Quicksand', 'sans-serif'],
+			},
+			colors: {
+				lightOrange: '#FFF0DE',
+				darkOrange: '#F9A109',
+				greyBorder: '#BDBDBD',
+			},
+		},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
+};
