18501abae8e2964f2bcec36b56c3084aa569e1b2
Ochuko Ekrresa <kelvinekrresa@gmail.com>
2021-01-09 04:39:05 +0100

    Populate shop when fetching products

--- server/src/api/repositories/productRepo.ts
+++ server/src/api/repositories/productRepo.ts
@@ -2,13 +2,21 @@ import { Product } from '../models/product';
 import { IProduct } from '../types';
 
 export async function getAllProducts() {
-	return await Product.find().limit(50).lean({ virtuals: true }).exec();
+	return await Product.find()
+		.limit(50)
+		.populate('shop', 'name country -_id')
+		.lean({ virtuals: true })
+		.exec();
 }
 
 export async function getProductsOfAShop(shopId: string) {
-	return await Product.find({ shop: shopId }).limit(50).lean({ virtuals: true }).exec();
+	return await Product.find({ shop: shopId })
+		.limit(50)
+		.populate('shop', 'name country -_id')
+		.lean({ virtuals: true })
+		.exec();
 }
 
 export async function insertProduct(product: IProduct) {
-	return await Product.create(product);
+	return await await Product.create(product);
 }
