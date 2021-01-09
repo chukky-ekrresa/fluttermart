fccaaa6cc1c2d2499656278441d8b88c3bd8c6ca
EstherSeyi <sheyiogundijo@gmail.com>
2021-01-09 22:08:22 +0100

    modifies auth action

--- frontend/src/redux/actions/auth.action.ts
+++ frontend/src/redux/actions/auth.action.ts
@@ -1,12 +1,12 @@
 import request from '../../utils/request';
-import { setAuthLoading, setAuthError } from '../reducers/auth.reduser';
+import { setAuthLoading, setAuthError, setUserID, setToken } from '../reducers/auth.reduser';
 
 export const login = (payload: any) => async (dispatch: any) => {
 	try {
+		dispatch(setAuthError(null));
 		dispatch(setAuthLoading(true));
 		const { data } = await request.post('auth/login', payload);
-
-		console.log(data);
+		dispatch(setToken(data?.data));
 		dispatch(setAuthLoading(false));
 	} catch (error) {
 		dispatch(setAuthLoading(false));
@@ -14,12 +14,34 @@ export const login = (payload: any) => async (dispatch: any) => {
 		console.log(error.response);
 	}
 };
-export const register = (payload: any) => async (dispatch: any) => {
+export const register = (payload: any, redirectToVerifyEmail: any) => async (dispatch: any) => {
 	try {
+		dispatch(setAuthError(null));
+		dispatch(setAuthLoading(true));
 		const { data } = await request.post('auth/register', payload);
+		dispatch(setUserID(data?.data));
+		dispatch(setAuthLoading(false));
 
-		console.log(data);
+		redirectToVerifyEmail(data?.data);
 	} catch (error) {
+		dispatch(setAuthLoading(false));
+		dispatch(setAuthError(error.response));
+		console.log(error.response);
+	}
+};
+
+export const verifyEmail = (payload: any) => async (dispatch: any) => {
+	try {
+		dispatch(setAuthError(null));
+		dispatch(setAuthLoading(true));
+		const { data } = await request.post('auth/verify_account', payload);
+
+		dispatch(setToken(data?.data));
+
+		dispatch(setAuthLoading(false));
+	} catch (error) {
+		dispatch(setAuthLoading(false));
+		dispatch(setAuthError(error.response));
 		console.log(error.response);
 	}
 };
