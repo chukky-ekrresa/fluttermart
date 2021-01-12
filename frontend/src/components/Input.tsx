066deb3fb7a9fd7c9aca4eb6353a501882d653fb
EstherSeyi <sheyiogundijo@gmail.com>
2021-01-12 02:04:51 +0100

    modifies input components

--- frontend/src/components/Input.tsx
+++ frontend/src/components/Input.tsx
@@ -8,7 +8,7 @@ const StyledInput = styled.input.attrs({
 type Props = {
 	label: string;
 	placeholder: string;
-	value: string;
+	value?: any;
 	name: string;
 	type: string;
 	onChange: (e: any) => void;
@@ -28,6 +28,37 @@ const Input = ({ label, placeholder, value, name, onChange, type }: Props) => {
 				name={name}
 				onChange={onChange}
 			/>
+		</div>
+	);
+};
+
+export const Select = ({}: any) => {
+	return (
+		<div className="mb-4">
+			<label className="block text-14 mb-2" htmlFor="country">
+				Country
+			</label>
+			<select className="border border-greyBorder outline-none focus:outline-none py-2.5 pl-4 rounded-md w-full">
+				<option>one</option>
+			</select>
+		</div>
+	);
+};
+export const TextArea = ({ label, placeholder, value, name, onChange }: any) => {
+	return (
+		<div className="mb-4">
+			<label className="block text-14 mb-2" htmlFor={name}>
+				{label}
+			</label>
+			<textarea
+				className="border border-greyBorder outline-none focus:outline-none py-2.5 pl-4 rounded-md w-full"
+				rows={3}
+				id={name}
+				placeholder={placeholder}
+				value={value}
+				name={name}
+				onChange={onChange}
+			></textarea>
 		</div>
 	);
 };
